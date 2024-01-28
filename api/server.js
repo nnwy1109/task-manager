2d8b5a34c9f0d17df697415643027230358f3814
Ng Wei Yang, Nigel <nigelng26@gmail.com>
2024-01-29 03:06:22 +0800

    refactor: include all changed files into commit

--- api/server.js
+++ api/server.js
@@ -7,21 +7,54 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(console.error);
+mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
+// Models
 const Todo = require('./models/Todo');
 
-app.get('/todos', async(req,res) => {
+app.get('/todos', async (req, res) => {
     const todos = await Todo.find();
 
     res.json(todos);
 });
 
-app.listen(3001, ()=> console.log("Server started on port 3001"));
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    })
 
+    todo.save();
 
+    res.json(todo);
+});
+
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+
+    res.json({result});
+});
+
+app.get('/todo/complete/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    todo.complete = !todo.complete;
+
+    todo.save();
+
+    res.json(todo);
+})
+
+app.put('/todo/update/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    todo.text = req.body.text;
+
+    todo.save();
+
+    res.json(todo);
+});
+
+app.listen(3001);
